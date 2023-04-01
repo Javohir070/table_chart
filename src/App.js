63adddb114e34f6ddd9347a1075aad2eb62052ca
Javohir <qayumovjavohir77@gmail.com>
2023-04-01 15:20:49 +0500

    hjdfghdsf

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './Components/Table/Table';
+import PolarAreaChart from './Components/Diya';
+function App() {
+  return (
+    <div className="App">
+      <Table />
+       <PolarAreaChart/>
+    </div>
+  );
+}
+
+export default App;
